db4455ff9f56ec88baca3f06b54f4cb6c9aed563
Fenix <gaiagaspar@gmail.com>
2020-10-27 11:25:09 -0300

    setup.py modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 # This file is part of the
 #   NirDust Project (https://github.com/Gaiana/nirdust)
-# Copyright (c) 2020, Gaiana Gaspar, Jose Alacoria
+# Copyright (c) 2020, Gaia Gaspar, Jose Alacoria
 # License: MIT
 #   Full Text: https://github.com/Gaiana/nirdust/LICENSE
 
@@ -24,10 +24,9 @@ import os
 import pathlib
 
 from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup
 
+use_setuptools()
 
 # =============================================================================
 # CONSTANTS
@@ -40,50 +39,38 @@ PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 with open(PATH / "README.md") as fp:
     LONG_DESCRIPTION = fp.read()
 
-with open(PATH / "grispy" / "__init__.py") as fp:
-    for line in fp.readlines():
-        if line.startswith("__version__ = "):
-            VERSION = line.split("=", 1)[-1].replace('"', '').strip()
-            break
 
-
-DESCRIPTION = "Grid Search in Python"
+DESCRIPTION = "Measure temperature of hot dust in type 2 AGN"
 
 
 # =============================================================================
 # FUNCTIONS
 # =============================================================================
 
+
 def do_setup():
     setup(
         name="nirdust",
-        version= "0.0.1",
-        description="Measure the temperature of the dust heated by an Active Galactic Nuclei (AGN) accretion disk.",
+        version="0.0.1",
+        description=DESCRIPTION,
         long_description=open("README.md").read(),
-        long_description_content_type='text/markdown',
-
-        author=[
-            "Gaiana Gaspar",
-            "Jose Alacoria"],
-        author_email="gaiagaspar@gmail.com",       
+        long_description_content_type="text/markdown",
+        author=["Gaia Gaspar", "Jose Alacoria"],
+        author_email="gaiagaspar@gmail.com",
         url="https://github.com/Gaiana/nirdust",
         py_modules=["nirdust", "ez_setup"],
         license="MIT",
-
-        keywords=["nirdust", "emission", "spectra", "nuclear", "temperatures"],
-
+        keywords=["nirdust", "dust", "AGN", "NIR", "temperatures"],
         classifiers=[
             "Intended Audience :: Education",
             "Intended Audience :: Science/Research",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 3.8"],
-
-        packages=["nirdust"],
-        py_modules=["ez_setup"],
-
-        install_requires=REQUIREMENTS)
+            "Programming Language :: Python :: 3.8",
+        ],
+        install_requires=REQUIREMENTS,
+    )
 
 
 if __name__ == "__main__":
