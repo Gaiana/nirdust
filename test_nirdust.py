e6594390ad527a61e0f1ffaf81abd1ec6fa742d2
Fenix <gaiagaspar@gmail.com>
2020-10-27 11:03:13 -0300

    added read_sample test

--- test_nirdust.py
+++ test_nirdust.py
@@ -22,17 +22,25 @@ TEST_PATH = pathlib.Path(PATH) / "test_data"
 # FIXTURES
 # ==============================================================================
 
+
 @pytest.fixture(scope="session")
 def cont03():
-    path = TEST_PATH / "cont03.fits"
-    #spect = nd.NuclearSpectrum(path, 0)
-    spect = nd.from_filename(path)
+    file_name = TEST_PATH / "cont03.fits"
+    spect = nd.read_single_spectrum(file_name, 0)
     return spect
+
+
+@pytest.fixture(scope="session")
+def sample1():
+    files = str(TEST_PATH / "cont*.fits")
+    lista = nd.read_sample(files, 0)
+    return lista
 
 
 # ==============================================================================
 # TESTS
 # ==============================================================================
+
 
 def test_match(cont03):
     spectrum = cont03
@@ -41,11 +49,16 @@ def test_match(cont03):
 
 def test_header(cont03):
     spectrum = cont03
-    assert isinstance(spectrum.header['EXPTIME'], (float, int))
-    assert spectrum.header['EXPTIME'] >= 0.
+    assert isinstance(spectrum.header["EXPTIME"], (float, int))
+    assert spectrum.header["EXPTIME"] >= 0.0
 
 
 def test_wav_axis(cont03):
     spectrum = cont03
-    assert spectrum.header['CD1_1'] >= 0.
-    assert spectrum.header['CTYPE1'] == 'LINEAR'
+    assert spectrum.header["CRVAL1"] >= 0.0
+    assert spectrum.header["CTYPE1"] == "LINEAR"
+
+
+def test_sample_size(sample1):
+    spec_list = sample1
+    assert len(spec_list) == 6
