c2c8bc64ac311c9bad406998b0053a383beefc4f
j0sealacoria <71361584+j0sealacoria@users.noreply.github.com>
2020-11-11 20:37:15 -0300

    Update nirdust.py

--- nirdust.py
+++ nirdust.py
@@ -30,6 +30,13 @@ import specutils.manipulation as sm
 
 @attr.s(frozen=True)
 class NirdustSpectrum:
+    """
+    Creat the class type NirdustSectrum.
+
+    Prepares the spectrum with specific methods, stores it in each instance of 
+    execution and then adjusts it.
+    
+    """
 
     header = attr.ib(repr=False)
     z = attr.ib()
@@ -41,9 +48,23 @@ class NirdustSpectrum:
     frequency_axis = attr.ib(repr=False)
 
     def __getattr__(self, a):
+        """Return objets after apply the "a" funcion"""
+        
         return getattr(self.spec1d, a)
 
     def __getitem__(self, slice):
+        """Defines the method that cuts the spectrum.
+
+        Parameters
+        ----------
+        slice: object
+            The object contain a NirdustSpectrum
+      
+        Return
+        ------
+        out: objets NirsdustSpectrum
+            Return a new instance of the class nirdustspectrum.
+        """
         spec1d = self.spec1d.__getitem__(slice)
         frequency_axis = spec1d.spectral_axis.to(u.Hz)
         kwargs = attr.asdict(self)
@@ -54,6 +75,18 @@ class NirdustSpectrum:
         return NirdustSpectrum(**kwargs)
 
     def cut_edges(self, mini, maxi):
+        """This funcion cuts the spectrum in wavelength range.
+
+        Parameters
+        ----------
+            mini: minimum wavelength to be cut
+            maxi: maximum wavelength to be cut
+
+        Return
+        ------
+        out: objets NirsdustSpectrum
+            Return a new instance of class nirdustspectrum cut in wavelength.
+        """
         region = su.SpectralRegion(mini * u.AA, maxi * u.AA)
         cutted_spec1d = sm.extract_region(self.spec1d, region)
         kwargs = attr.asdict(self)
@@ -63,6 +96,14 @@ class NirdustSpectrum:
         return NirdustSpectrum(**kwargs)
 
     def _convert_to_frequency(self):
+        """Converts the spectral axis to frequency in units of GHz.
+
+        Return
+        ------
+        out: objets NirsdustSpectrum
+            New instance of the nirdustSpectrun classe containing spectrum in
+            frequencies.
+        """ 
         new_axis = self.spec1d.spectral_axis.to(u.GHz)
         kwargs = attr.asdict(self)
         kwargs.update(
@@ -71,6 +112,15 @@ class NirdustSpectrum:
         return NirdustSpectrum(**kwargs)
 
     def _normalization(self):
+        """This function normalizes the spectrum to the mean value.
+
+         
+        Return
+        ------
+        out: objets NirsdustSpectrum
+            New instance of the nirdustSpectrun classe whose flow is normalized
+            to the average value.
+        """  
         normalized_flux = self.spec1d.flux / np.mean(self.spec1d.flux)
         new_spec1d = su.Spectrum1D(normalized_flux, self.spec1d.spectral_axis)
         kwargs = attr.asdict(self)
@@ -92,7 +142,24 @@ def spectrum(
     dispersion_type="CTYPE1",
     **kwargs,
 ):
+    """This funtion takes the parameters of the spectrum and passes them on to 
+    nerdustspectrum class.
 
+    Parameters
+    ----------
+    flux: Intensity for each pixel in arbitrary units
+    header: Header of spectrum
+    z: Redshift
+    dispersion_key: keywords that gives dispersion in Å/pix
+    first_wavelength: keywords that gives wavelength of first pixel
+    dispersion_type: keywords that gives order the dispersion function
+     
+
+    Return
+    ------
+    out: objets NirsdustSpectrum
+        Return a class of nerdustspectrum with the entered parameters.
+    """
     if header[dispersion_key] <= 0:
         raise ValueError("dispersion must be positive")
 
@@ -123,6 +190,19 @@ def spectrum(
 
 
 def read_spectrum(file_name, extension, z, **kwargs):
+    """Read a spectrum in fits format.
+
+    Parameters
+    ----------
+    file_name: Path to where the fits file is stored
+    extension: File extension fits
+    z: Redshift
+      
+    Return
+    ------
+    out: objets NirsdustSpectrum
+        Return a new instance of the class nirdustspectrum with the stored spectrum.
+    """
 
     with fits.open(file_name) as fits_spectrum:
 
