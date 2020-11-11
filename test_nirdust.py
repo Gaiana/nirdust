1e1e6dd478201bb445ec7e58be9416663c9f75a7
Fenix <gaiagaspar@gmail.com>
2020-11-11 18:57:42 -0300

    added tests fot Nirdustprepare function

--- test_nirdust.py
+++ test_nirdust.py
@@ -31,7 +31,21 @@ TEST_PATH = pathlib.Path(PATH) / "test_data"
 @pytest.fixture(scope="session")
 def NGC4945_continuum():
     file_name = TEST_PATH / "cont03.fits"
-    spect = nd.read_spectrum(file_name, 0, 0.01)
+    spect = nd.read_spectrum(file_name, 0, 0.00188)
+    return spect
+
+
+@pytest.fixture(scope="session")
+def NGC4945_external_continuum_400pc():
+    file_name = TEST_PATH / "external_spectrum_400pc_N4945.fits"
+    spect = nd.read_spectrum(file_name, 0, 0.00188)
+    return spect
+
+
+@pytest.fixture(scope="session")
+def NGC4945_external_continuum_200pc():
+    file_name = TEST_PATH / "external_spectrum_200pc_N4945.fits"
+    spect = nd.read_spectrum(file_name, 0, 0.00188)
     return spect
 
 
@@ -67,15 +81,15 @@ def test_redshift_correction(NGC4945_continuum):
     spectrum = NGC4945_continuum
     assert (
         spectrum.spectral_axis[0]
-        == (spectrum.header["CRVAL1"] / (1 + 0.01)) * u.AA
+        == (spectrum.header["CRVAL1"] / (1 + 0.00188)) * u.AA
     )
 
 
 def test_convert_to_frequency(NGC4945_continuum):
     spectrum = NGC4945_continuum
-    freq = spectrum.convert_to_frequency().frequency_axis
+    freq = spectrum._convert_to_frequency().frequency_axis
     np.testing.assert_almost_equal(
-        freq.value.mean(), 138426.2092285212, decimal=7
+        freq.value.mean(), 137313.317328585, decimal=7
     )
 
 
@@ -102,6 +116,26 @@ def test_cut_edges(NGC4945_continuum):
 
 def test_nomrmalization(NGC4945_continuum):
     spectrum = NGC4945_continuum
-    normalized_spectrum = spectrum.normalization()
+    normalized_spectrum = spectrum._normalization()
     mean = np.mean(normalized_spectrum.spec1d.flux)
     assert mean == 1.0
+
+
+def test_nirdustprepare(NGC4945_continuum, NGC4945_external_continuum_400pc):
+    spectrum = NGC4945_continuum
+    external_spectrum = NGC4945_external_continuum_400pc
+    prepared = nd.Nirdustprepare(spectrum, external_spectrum, 19600, 22900)
+    cutted_spectrum = spectrum.cut_edges(19600, 22900)
+    expected_len = len(cutted_spectrum.flux)
+    assert len(prepared.flux) == expected_len
+
+
+def test_nirdustprepare_second_if(
+    NGC4945_continuum, NGC4945_external_continuum_200pc
+):
+    spectrum = nd.read_spectrum(TEST_PATH / "cont01.fits", 0, 0.00188)
+    external_spectrum = NGC4945_external_continuum_200pc
+    prepared = nd.Nirdustprepare(spectrum, external_spectrum, 19600, 22900)
+    cutted_spectrum = spectrum.cut_edges(19600, 22900)
+    expected_len = len(cutted_spectrum.flux)
+    assert len(prepared.flux) == expected_len
