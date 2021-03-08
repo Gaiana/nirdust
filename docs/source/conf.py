c1f564d6e96e1e15a145b7be7a399bb1531ad14f
Gaiana <71792014+Gaiana@users.noreply.github.com>
2021-03-08 11:09:03 -0300

    added text to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,10 +47,17 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md']
+
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
