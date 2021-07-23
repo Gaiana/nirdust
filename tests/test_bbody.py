2dcd2094e3e5fd1681dc7b9ecd500351a98d08b0
Fenix <gaiagaspar@gmail.com>
2021-07-23 11:41:43 -0300

    test for NirdustFitter plot

--- tests/test_bbody.py
+++ tests/test_bbody.py
@@ -19,7 +19,7 @@ from astropy.modeling.models import BlackBody
 
 from matplotlib.testing.decorators import check_figures_equal
 
-from nirdust import bbody, core, preprocessing
+from nirdust import bbody, core
 
 import numpy as np
 
@@ -95,6 +95,74 @@ def test_fit_error_2(NGC4945_continuum):
         fitter.fit(initial_state=initial_st)
 
 
+@check_figures_equal()
+def test_plot(fig_test, fig_ref, NGC4945_continuum):
+
+    spectrum = NGC4945_continuum.cut_edges(19500, 22900)
+
+    # BlackBody model
+    true_T = 1233 * u.K
+    true_scale = 23.0
+    model = models.BlackBody(true_T, scale=true_scale)
+    bb = model(spectrum.frequency_axis).value
+
+    # Linear model
+    def tp_line(x, x1, x2, y1, y2):
+        return (y2 - y1) / (x2 - x1) * (x - x1) + y1
+
+    wave = spectrum.spectral_axis.value
+    delta_bb = bb[-1] - bb[0]
+    y1_line, y2_line = bb[0] + 2 / 3 * delta_bb, bb[0] + 1 / 3 * delta_bb
+    line = tp_line(wave, wave[0], wave[-1], y1_line, y2_line)
+
+    # Total model
+    flux = line * u.adu + bb * u.adu
+
+    spectrumT = core.NirdustSpectrum(
+        flux=flux, spectral_axis=spectrum.spectral_axis
+    )
+    externalT = core.NirdustSpectrum(
+        flux=33 * line * u.adu, spectral_axis=spectrum.spectral_axis
+    )
+
+    fitter = bbody.NirdustFitter(spectrumT, externalT)
+    fitter.fit(steps=20)
+
+    # test figure is generated
+    ax_test = fig_test.subplots(2, 1, sharex=True)
+    fitter.plot(ax=ax_test)
+
+    # ref figure is constructed
+
+    ax_ref = fig_ref.subplots(2, 1, sharex=True)
+
+    ax_t, ax_log = ax_ref
+    fig = ax_t.get_figure()
+    fig.subplots_adjust(hspace=0)
+
+    chain = fitter.chain(discard=0)
+    arr_t = chain[:, :, 0]
+    mean_t = arr_t.mean(axis=1)
+
+    arr_log = chain[:, :, 1]
+    mean_log = arr_log.mean(axis=1)
+
+    # plot
+    ax_t.set_title(
+        f"Sampled parameters\n Steps={fitter.steps_} - Discarded={0}"
+    )
+
+    ax_t.plot(arr_t, alpha=0.5)
+    ax_t.plot(mean_t, color="k", label="Mean")
+    ax_t.set_ylabel("T")
+
+    ax_log.plot(arr_log, alpha=0.5)
+    ax_log.plot(mean_log, color="k", label="Mean")
+    ax_log.set_ylabel("log(scale)")
+    ax_log.set_xlabel("Steps")
+    ax_log.legend()
+
+
 # =============================================================================
 # BLACKBODY RESULT
 # =============================================================================
@@ -129,75 +197,6 @@ def test_NirdustResults_flux_axis(NGC4945_continuum):
         20 * u.K, 25, fitted_blackbody=None, dust=spectrum
     )
     assert spectrum == nr_inst.dust
-
-
-# =============================================================================
-# FIT BLACK BODY
-# =============================================================================
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize("true_temp", [500.0, 1000.0, 5000.0])
-@pytest.mark.parametrize("scaling", ["downscale", "upscale"])
-def test_fit_blackbody_with_resampling(
-    NGC4945_continuum_rest_frame,
-    NGC3998_sp_lower_resolution,
-    true_temp,
-    scaling,
-):
-    real_spectrum = NGC4945_continuum_rest_frame
-    freq_axis = real_spectrum.frequency_axis
-    sinthetic_model = BlackBody(true_temp * u.K)
-    sinthetic_flux = sinthetic_model(freq_axis)
-
-    # the core.NirdustSpectrum object is instantiated
-    snth_blackbody = core.NirdustSpectrum(
-        flux=sinthetic_flux,
-        spectral_axis=real_spectrum.spectral_axis,
-        z=0,
-    )
-
-    # resampling
-    f_sp, s_sp = preprocessing.match_spectral_axes(
-        snth_blackbody, NGC3998_sp_lower_resolution, scaling=scaling
-    )
-    snth_bb_temp = bbody.fit_blackbody(
-        f_sp.normalize().convert_to_frequency(), 2000.0
-    ).temperature
-
-    np.testing.assert_almost_equal(snth_bb_temp.value, true_temp, decimal=1)
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize("true_temp", [500.0, 1000.0, 5000.0])
-@pytest.mark.parametrize("scaling", ["downscale", "upscale"])
-def test_fit_blackbody_with_resampling_in_inverse_order(
-    NGC4945_continuum_rest_frame,
-    NGC3998_sp_lower_resolution,
-    true_temp,
-    scaling,
-):
-    real_spectrum = NGC4945_continuum_rest_frame
-    freq_axis = real_spectrum.frequency_axis
-    sinthetic_model = BlackBody(true_temp * u.K)
-    sinthetic_flux = sinthetic_model(freq_axis)
-
-    # the core.NirdustSpectrum object is instantiated
-    snth_blackbody = core.NirdustSpectrum(
-        flux=sinthetic_flux,
-        spectral_axis=real_spectrum.spectral_axis,
-        z=0,
-    )
-
-    # resampling but inverting the input order than prevoius test
-    _, s_sp = preprocessing.match_spectral_axes(
-        NGC3998_sp_lower_resolution, snth_blackbody, scaling=scaling
-    )
-    snth_bb_temp = bbody.fit_blackbody(
-        s_sp.normalize().convert_to_frequency(), 2000.0
-    ).temperature
-
-    np.testing.assert_almost_equal(snth_bb_temp.value, true_temp, decimal=1)
 
 
 # =============================================================================
