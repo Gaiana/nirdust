898e9eece00ba386e7bfd073fcca6c65e7123d2a
Fenix <gaiagaspar@gmail.com>
2021-07-23 12:14:19 -0300

    added test for plot in NirdustFitter

--- tests/test_bbody.py
+++ tests/test_bbody.py
@@ -163,6 +163,75 @@ def test_plot(fig_test, fig_ref, NGC4945_continuum):
     ax_log.legend()
 
 
+
+@check_figures_equal()
+def test_plot(fig_test, fig_ref, NGC4945_continuum):
+
+    spectrum = NGC4945_continuum.cut_edges(19500, 22900)
+
+    # BlackBody model
+    true_T = 1233 * u.K
+    true_scale = 23.0
+    model = models.BlackBody(true_T, scale=true_scale)
+    bb = model(spectrum.frequency_axis).value
+
+    # Linear model
+    def tp_line(x, x1, x2, y1, y2):
+        return (y2 - y1) / (x2 - x1) * (x - x1) + y1
+
+    wave = spectrum.spectral_axis.value
+    delta_bb = bb[-1] - bb[0]
+    y1_line, y2_line = bb[0] + 2 / 3 * delta_bb, bb[0] + 1 / 3 * delta_bb
+    line = tp_line(wave, wave[0], wave[-1], y1_line, y2_line)
+
+    # Total model
+    flux = line * u.adu + bb * u.adu
+
+    spectrumT = core.NirdustSpectrum(
+        flux=flux, spectral_axis=spectrum.spectral_axis
+    )
+    externalT = core.NirdustSpectrum(
+        flux=33 * line * u.adu, spectral_axis=spectrum.spectral_axis
+    )
+
+    fitter = bbody.NirdustFitter(spectrumT, externalT)
+    fitter.fit(steps=20)
+
+    # test figure is generated
+    ax_test = fig_test.subplots(2,1, sharex=True)
+    with patch("matplotlib.pyplot.subplots", return_value=(fig_test,ax_test)):
+        fitter.plot()
+
+    # ref figure is constructed
+
+    ax_ref = fig_ref.subplots(2, 1, sharex=True)
+
+    ax_t, ax_log = ax_ref
+    fig = ax_t.get_figure()
+    fig.subplots_adjust(hspace=0)
+
+    chain = fitter.chain(discard=0)
+    arr_t = chain[:, :, 0]
+    mean_t = arr_t.mean(axis=1)
+
+    arr_log = chain[:, :, 1]
+    mean_log = arr_log.mean(axis=1)
+
+    # plot
+    ax_t.set_title(
+        f"Sampled parameters\n Steps={fitter.steps_} - Discarded={0}"
+    )
+
+    ax_t.plot(arr_t, alpha=0.5)
+    ax_t.plot(mean_t, color="k", label="Mean")
+    ax_t.set_ylabel("T")
+
+    ax_log.plot(arr_log, alpha=0.5)
+    ax_log.plot(mean_log, color="k", label="Mean")
+    ax_log.set_ylabel("log(scale)")
+    ax_log.set_xlabel("Steps")
+    ax_log.legend()
+
 # =============================================================================
 # BLACKBODY RESULT
 # =============================================================================
